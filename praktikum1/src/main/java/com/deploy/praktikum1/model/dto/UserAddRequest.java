fdfdc2651605fdf17f1e476f820543ef183ceccb
nachool <nachma.olivia1394@gmail.com>
2026-03-03 15:19:15 +0700

    revisi add req

--- praktikum1/src/main/java/com/deploy/praktikum1/model/dto/UserAddRequest.java
+++ praktikum1/src/main/java/com/deploy/praktikum1/model/dto/UserAddRequest.java
@@ -2,12 +2,10 @@ package com.deploy.praktikum1.model.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
 public class UserAddRequest {
-    @NotBlank
     private String name;
-
-    @Email
-    @NotBlank
-    private String email;
+    private String age;
 }
