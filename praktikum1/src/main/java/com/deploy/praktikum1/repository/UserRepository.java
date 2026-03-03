06ddbb27d3da9ce5fe16d7882ecdf76cb86cd508
nachool <nachma.olivia1394@gmail.com>
2026-03-03 14:26:38 +0700

    menambahkan interface UserRepository

---
+++ praktikum1/src/main/java/com/deploy/praktikum1/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.deploy.praktikum1.repository;
+
+import com.deploy.praktikum1.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
