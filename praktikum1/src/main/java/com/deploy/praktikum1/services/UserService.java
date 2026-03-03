c1e9aa0df2ed4e60dbd7220b0f980fbb56a9b8a8
nachool <nachma.olivia1394@gmail.com>
2026-03-03 14:35:41 +0700

    menambahkan interface UserService

--- praktikum1/src/main/java/com/deploy/praktikum1/services/UserService.java
+++ praktikum1/src/main/java/com/deploy/praktikum1/services/UserService.java
@@ -1,4 +1,17 @@
 package com.deploy.praktikum1.services;
 
+import com.deploy.praktikum1.model.dto.UserAddRequest;
+import com.deploy.praktikum1.model.dto.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+
+    UserDto create(UserAddRequest request);
+
+    List<UserDto> findAll();
+
+    UserDto findById(Long id);
+
+    void delete(Long id);
 }
